1107d921bea85a8aecdba7a85a1b9b6ad293c392
Ivan Turygin <turyginkovrov@gmail.com>
2024-05-19 18:35:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+   direction: 'horizontal',
+
+   slidesPerView: 3,
+	spaceBetween: 30,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
